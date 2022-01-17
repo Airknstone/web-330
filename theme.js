83f79a3d5b5cbccb2f838d366452cde19072f8d7
Arkenstone <trejo.allan@yahoo.com>
2022-01-16 22:51:48 -0800

    updated theme

--- theme.js
+++ theme.js
@@ -7,20 +7,17 @@ Description: This script uses localStorage api to toggle set default Theme
 
 //  expected output Storage {mode: 'light-theme', iconMode: 'fa-toggle-off', iconText: 'Light Mode'}
 //
-function setDefaultTheme ()
-{
-    localStorage.clear();
-    const theme = localStorage.getItem( 'mode' ) || 'light-theme';
-    const iconMode = localStorage.getItem( 'iconMode' ) || 'fa-toggle-off';
-    const iconText = localStorage.getItem( 'iconText' ) || 'Light Mode';
+function setDefaultTheme() {
+  const theme = localStorage.getItem('mode') || 'light-theme';
+  const iconMode = localStorage.getItem('iconMode') || 'fa-toggle-off';
+  const iconText = localStorage.getItem('iconText') || 'Light Mode';
 
-    document.body.classList = theme;
-    document.getElementById( 'icon-mode' ).classList.add( iconMode );
-    document.getElementById( 'icon-text' ).innerHTML = iconText;
-
+  document.body.classList = theme;
+  document.getElementById('icon-mode').classList.add(iconMode);
+  document.getElementById('icon-text').innerHTML = iconText;
 }
 
-function setSelectedTheme()
-{
-    document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+function setSelectedTheme() {
+  document.body.classList.value = localStorage.getItem('mode') || 'light-theme';
+  console.log(localStorage);
 }

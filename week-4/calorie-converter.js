b3d60cc7394e9f0438225998f1a82d0385063709
Arkenstone <trejo.allan@yahoo.com>
2022-01-29 23:09:09 -0800

    Added Comments

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -1,6 +1,13 @@
+// calorie-converter.js
+// Author: Professor Krasso
+// Modified by: Allan Trejo
+// Date: 01-27-2022
+// Description: Static methods are called directly on the class (Car from the example above) -
+//  without creating an instance/object (mycar) of the class.
 import { FoodModel } from './food-model.js';
 
 export class CalorieConverter {
+  // Static array of FoodModel Objects, contains 6 items
   static data = [
     new FoodModel(1007, 'Egg', 78),
     new FoodModel(1008, 'Apple', 95),
@@ -9,6 +16,8 @@ export class CalorieConverter {
     new FoodModel(1011, 'Banana', 105),
     new FoodModel(1012, 'Soda', 150),
   ];
+  // Expected Result: Egg returns data[0]
+  // Returns an array of objects if the object.name is included in user input
   static find(str) {
     return this.data.filter((data) => {
       if (str.includes(data.name.toLowerCase())) {
--- week-4/food-model.js
+++ week-4/food-model.js
@@ -1,3 +1,8 @@
+// Title: food-models.js
+// Author: Professor Krasso
+// Modified by: Allan Trejo
+// Date: 01-27-2022
+// Description: Simple Class
 export class FoodModel {
   constructor(id, name, calories) {
     this.id = id;
